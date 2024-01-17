9fd8be173711c6a6dcad3c252da45046a30b892f
CYTech Student <abousoufie@cy-tech.fr>
2024-01-17 13:01:17 +0100

    verif experience dates

--- application/views/user/profil.php
+++ application/views/user/profil.php
@@ -370,11 +370,11 @@ foreach ($experiences as $index => $experience) {
                         <input type="text" id="userExperienceCompany" name="userExperienceCompany" value="<?=$experience->experienceCompany?>" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Entreprise">
 
                         <label for="userExperienceDateDebut" class="w-full text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Date de début</label>
-                        <input type="date" id="updateUserExperienceDateDebut" name="userExperienceDateDebut" value="<?=$experience->experienceDateDebut?>" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <input type="date" id="updateUserExperienceDateDebut<?=$index?>" name="userExperienceDateDebut" value="<?=$experience->experienceDateDebut?>" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onkeyup="validateDates('updateUserExperienceDateDebut<?=$index?>', 'updateUserExperienceDateFin<?=$index?>', 'errorUpdateUserExperienceDate<?=$index?>', 'userExperienceSubmit<?=$index?>')">
 
                         <label for="userExperienceDateFin" class="text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Date de fin</label>
-                        <input type="date" id="updateUserExperienceDateFin" name="userExperienceDateFin" value="<?=$experience->experienceDateFin?>" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <p id="errorUpdateUserExperienceDate" class="text-red-500" style="display:none;">La date de fin doit être postérieure à la date de début</p>
+                        <input type="date" id="updateUserExperienceDateFin<?=$index?>" name="userExperienceDateFin" value="<?=$experience->experienceDateFin?>" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onkeyup="validateDates('updateUserExperienceDateDebut<?=$index?>', 'updateUserExperienceDateFin<?=$index?>', 'errorUpdateUserExperienceDate<?=$index?>', 'userExperienceSubmit<?=$index?>')">
+                        <p id="errorUpdateUserExperienceDate<?=$index?>" class="text-red-500" style="display:none;">La date de fin doit être postérieure à la date de début</p>
 
                         <label for="userExperienceDescription" class="text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Description</label>
                         <textarea id="userExperienceDescription" name="userExperienceDescription" rows="3" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"><?=$experience->experienceDescription?></textarea>
@@ -412,7 +412,7 @@ foreach ($experiences as $index => $experience) {
 
                     <div class="flex items-center justify-between mt-4">
                         <div>
-                            <button type="submit" class="text-2xl lg:text-base text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg  px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            <button type="submit" id="userExperienceSubmit<?=$index?>" class="text-2xl lg:text-base text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg  px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                 Valider
                             </button>
                             <button type="button" data-modal-toggle="updateUserExperience<?=$index?>" class="text-2xl lg:text-base text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg  px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
@@ -460,7 +460,7 @@ foreach ($experiences as $index => $experience) {
                 </button>
             </div>
             <!-- Modal body -->
-            <form action="<?=base_url("user/addUserExperience")?>" method="post" enctype="multipart/form-data">
+            <form id="addUserExperience-form" action="<?=base_url("user/addUserExperience")?>" method="post" enctype="multipart/form-data">
                 <div>
                     <label for="userExperienceJob" class="text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Nom de l'experience</label>
                     <input type="text" id="userExperienceJob" name="userExperienceJob" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Titre" required>
@@ -468,11 +468,12 @@ foreach ($experiences as $index => $experience) {
                     <input type="text" id="userExperienceCompany" name="userExperienceCompany" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Entreprise" required>
 
                     <label for="userExperienceDateDebut" class="text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Date de début</label>
-                    <input type="date" id="userExperienceDateDebut" name="userExperienceDateDebut" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    <input type="date" id="addUserExperienceDateDebut" name="userExperienceDateDebut" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
 
                     <label for="userExperienceDateFin" class="text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Date de fin</label>
-                    <input type="date" id="userExperienceDateFin" name="userExperienceDateFin" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                    
+                    <input type="date" id="addUserExperienceDateFin" name="userExperienceDateFin" class="text-2xl lg:text-base bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    <p id="errorAddUserExperienceDate" class="mt-2 text-red-500" style="display:none;">La date de fin doit être postérieure à la date de début</p>
+
                     <label for="userExperienceDescription" class="text-2xl lg:text-base block mt-4 mb-2  font-medium text-gray-900 dark:text-white">Description</label>
                     <textarea id="userExperienceDescription" name="userExperienceDescription" rows="3" class="text-2xl lg:text-basebg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required></textarea>
 
@@ -1949,23 +1950,25 @@ document.addEventListener('DOMContentLoaded', function() {
         $("#fileSizeInfo").text("La taille maximale autorisée est " + maxSizeInMB + " Mo.");
     });
 
-    document.getElementById('updateUserExperience-form').addEventListener('submit', function(event) {
-        var startDate = new Date(document.getElementById('updateUserExperienceDateDebut').value);
-        var endDate = new Date(document.getElementById('updateUserExperienceDateFin').value);
-        var errorMessage = document.getElementById('errorUpdateUserExperienceDate');
+    function validateDates(dateDebut, dateFin, errorMessageDate, submitButton) {
+        var startDate = new Date(document.getElementById(dateDebut).value);
+        var endDate = new Date(document.getElementById(dateFin).value);
+        var errorMessage = document.getElementById(errorMessageDate);
+        var submitBtn = document.getElementById(submitButton);
 
-        if (startDate > endDate) {
-            errorMessage.style.display = 'block';
-            event.preventDefault(); // Empêcher la soumission du formulaire
+        if (startDate && endDate && startDate > endDate) {
+            errorMessage.style.display = "block";
+            submitBtn.disabled = true;
         } else {
-            errorMessage.style.display = 'none';
+            errorMessage.style.display = "none";
+            submitBtn.disabled = false;
         }
-    });
-
-    $('#updateUserExperienceDateDebut, #updateUserExperienceDateFin').on('change keyup', function() {
-        var startDate = new Date($('#updateUserExperienceDateDebut').val());
-        var endDate = new Date($('#updateUserExperienceDateFin').val());
-        var errorMessage = $('#errorUpdateUserExperienceDate');
+    }
+    
+    $('#addUserExperienceDateDebut, #addUserExperienceDateFin').on('change keyup', function() {
+        var startDate = new Date($('#addUserExperienceDateDebut').val());
+        var endDate = new Date($('#addUserExperienceDateFin').val());
+        var errorMessage = $('#errorAddUserExperienceDate');
 
         if (startDate && endDate && startDate > endDate) {
             errorMessage.show();
@@ -1974,12 +1977,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    $('#updateUserExperience-form').on('submit', function(event) {
-        var startDate = new Date($('#updateUserExperienceDateDebut').val());
-        var endDate = new Date($('#updateUserExperienceDateFin').val());
+    $('#addUserExperience-form').on('submit', function(event) {
+        var startDate = new Date($('#addUserExperienceDateDebut').val());
+        var endDate = new Date($('#addUserExperienceDateFin').val());
 
         if (startDate && endDate && startDate > endDate) {
-            $('#errorUpdateUserExperienceDate').show();
+            $('#errorAddUserExperienceDate').show();
             event.preventDefault(); // Empêcher la soumission du formulaire
         }
     });

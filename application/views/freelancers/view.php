8c1551904e2ae2233aaff098e1b33d2d33407432
CYTech Student <abousoufie@cy-tech.fr>
2024-01-24 17:03:06 +0100

    set rating minimum to 1/5

--- application/views/freelancers/view.php
+++ application/views/freelancers/view.php
@@ -75,14 +75,14 @@ include(APPPATH . 'views/layouts/company/header.php' );
                 <div class="flex items-center">                    
                 <label for="ratingStars" class="block mr-4 font-medium text-gray-900 dark:text-white">Votre note : </label>
 
-                <img src="<?php echo base_url('assets/img/light-star.svg'); ?>" class="star w-6 h-6" onclick="setRating(1)">
+                <img src="<?php echo base_url('assets/img/fill-star.svg'); ?>" class="star w-6 h-6" onclick="setRating(1)">
                 <img src="<?php echo base_url('assets/img/light-star.svg'); ?>" class="star w-6 h-6" onclick="setRating(2)">
                 <img src="<?php echo base_url('assets/img/light-star.svg'); ?>" class="star w-6 h-6" onclick="setRating(3)">
                 <img src="<?php echo base_url('assets/img/light-star.svg'); ?>" class="star w-6 h-6" onclick="setRating(4)">
                 <img src="<?php echo base_url('assets/img/light-star.svg'); ?>" class="star w-6 h-6" onclick="setRating(5)">
                 </div>
 
-                <input type="text" name="ratingStars" id="ratingStars" value="0" class="hidden mb-2 bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                <input type="text" name="ratingStars" id="ratingStars" value="1" class="hidden mb-2 bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
 
                     
                     <label for="ratingComment" class="block mt-4 mb-2 font-medium text-gray-900 dark:text-white">Votre commentaire</label>

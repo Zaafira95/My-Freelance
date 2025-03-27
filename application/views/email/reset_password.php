fc47fedd4fc3a108a37c86f280d2ed468ca074dd
CYTech Student <abousoufie@cy-tech.fr>
2025-03-27 14:41:40 +0100

    emails traduction done

--- application/views/email/reset_password.php
+++ application/views/email/reset_password.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title> Réinitialisation de votre mot de passe </title>
+    <title> Reset your password </title>
 
 
 <style>
@@ -31,27 +31,26 @@
 
         <img src="https://uploads-ssl.webflow.com/636a2ecc04b5b050d3f252f9/65b920d2b1180ca9828c6e9c_CC-Logo-Prod.png" alt="My Freelance" width="600" style="width:200px;margin-top:20px; margin-bottom:40px;display:block; margin-left:0px; margin-right:auto;">
 
-        <h1 style="font-size: 24px; font-weight: 600; color: #000000; margin-top: 20px; margin-bottom: 20px; padding: 0; text-align: left;">Réinitialisez votre mot de passe</h1>
+        <h1 style="font-size: 24px; font-weight: 600; color: #000000; margin-top: 20px; margin-bottom: 20px; padding: 0; text-align: left;">Reset your password</h1>
 
         <!-- Activate your account text -->
-        <p style="font-size: 16px; font-weight: 400; color: #000000; margin: 0; padding: 0; text-align: left;">Nous avons récemment reçu une demande de réinitialisation de mot de passe associée à votre compte. </p>
-        <p style="font-size: 16px; font-weight: 400; color: #000000; margin: 0; padding: 0; text-align: left;"> Si vous êtes à l'origine de cette demande, veuillez confirmer en cliquant sur le lien ci-dessous. Veuillez noter que, par mesure de sécurité, ce lien ne sera actif que pendant 24 heures</p>
-
+        <p style="font-size: 16px; font-weight: 400; color: #000000; margin: 0; padding: 0; text-align: left;">We recently received a request to reset the password associated with your account. </p>
+        <p style="font-size: 16px; font-weight: 400; color: #000000; margin: 0; padding: 0; text-align: left;"> If you initiated this request, please confirm by clicking the link below. Please note that, for security reasons, this link will only be active for 24 hours.</p>
 
         <!-- Activate your account button -->
-        <a href="<?php echo $resetPasswordLink; ?>" style="background-color: #3971FA; border-radius:40px; color: #ffffff; display: block; font-size: 16px; font-weight: 600; line-height: 1.5; margin-bottom:24px; margin-top: 24px; margin-left:auto; margin-right:auto; max-width: 320px; padding: 12px 24px; text-align: center; text-decoration: none; width: 100%;">Réinitialiser Mon Mot de Passe</a>
+        <a href="<?php echo $resetPasswordLink; ?>" style="background-color: #3971FA; border-radius:40px; color: #ffffff; display: block; font-size: 16px; font-weight: 600; line-height: 1.5; margin-bottom:24px; margin-top: 24px; margin-left:auto; margin-right:auto; max-width: 320px; padding: 12px 24px; text-align: center; text-decoration: none; width: 100%;">Reset my password</a>
 
-        <p style="font-size: 12px; font-weight: 400; color: #000000; margin-top: 20px; padding: 0; text-align: left;"> Si vous n'êtes pas à l'origine de cette demande, veuillez ignorer ce message. Aucune action supplémentaire n'est requise de votre part.</p>
+        <p style="font-size: 12px; font-weight: 400; color: #000000; margin-top: 20px; padding: 0; text-align: left;"> If you did not make this request, please ignore this message. No further action is required on your part.</p>
 
 
-        <p style="font-size: 14px; font-weight: 200; color: #000000; margin-top: 20px; padding: 0; text-align: left;">A bientôt, <br> L'équipe My Freelance ☕️</p>
+        <p style="font-size: 14px; font-weight: 200; color: #000000; margin-top: 20px; padding: 0; text-align: left;">See you soon, <br> My Freelance Team ☕️</p>
 
 
     </div>  
     <!-- Footer -->
     <div style="border-top: 1px solid #E5E5E5; margin-top: 40px; padding-top:20px;">
         <p style="font-size: 14px; font-weight: 100; color: #000000; margin: 0; padding: 0; text-align: center;">My Freelance</p>
-        <p style="font-size: 14px; font-weight: 100; color: #000000; margin: 0; padding: 0; text-align: center;">© 2024 My Freelance. Tous droits réservés.</p>
+        <p style="font-size: 14px; font-weight: 100; color: #000000; margin: 0; padding: 0; text-align: center;">© 2024 My Freelance. All rights reserved.</p>
         <!-- Logo instagram, youtube and linkedin -->
         <div style="text-align:center; margin-top:20px;">
             <a href="https://www.instagram.com/cafecreme_agency" style="margin-right: 10px;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png" alt="Instagram" width="30" style="width:30px;"></a>
--- application/views/email/welcome_email_freelance.php
+++ application/views/email/welcome_email_freelance.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title> Bienvenue chez My Freelance </title>
+    <title> Welcome to My Freelance </title>
 
 
 <style>
@@ -31,11 +31,11 @@
 
         <img src="https://uploads-ssl.webflow.com/636a2ecc04b5b050d3f252f9/65b920d2b1180ca9828c6e9c_CC-Logo-Prod.png" alt="My Freelance" width="600" style="width:200px;margin-top:20px; margin-bottom:40px;display:block; margin-left:0px; margin-right:auto;">
 
-        <h1 style="font-size: 24px; font-weight: 600; color: #000000; margin-top: 20px; margin-bottom: 20px; padding: 0; text-align: left;">Bienvenue chez My Freelance 👋🏻 </h1>
+        <h1 style="font-size: 24px; font-weight: 600; color: #000000; margin-top: 20px; margin-bottom: 20px; padding: 0; text-align: left;">Welcome to My Freelance 👋🏻 </h1>
 
-        <p style="font-size: 14px; font-weight: 400; color: #000000; margin-top: 20px; padding: 0; text-align: left;">Hello <?php echo $userFirstName ." ".$userLastName?>, nous sommes ravis de te compter parmi nous !</p>
+        <p style="font-size: 14px; font-weight: 400; color: #000000; margin-top: 20px; padding: 0; text-align: left;">Hello <?php echo $userFirstName ." ".$userLastName?>, we're excited to have you on board!</p>
         
-        <p style="font-size: 14px; font-weight: 400; color: #000000; margin-top: 20px; padding: 0; text-align: left;">Prêt à booster ta carrière IT ? Crée ton profil dès maintenant, donne tes dispos, et choisis ta visibilité auprès des recruteurs. Découvre nos offres de missions freelance 100% IT 🚀</p>
+        <p style="font-size: 14px; font-weight: 400; color: #000000; margin-top: 20px; padding: 0; text-align: left;">Ready to boost your IT career? Create your profile now, provide your availability, and choose your visibility with recruiters. Discover our 100% IT freelance job opportunities. 🚀</p>
 
 
 
@@ -43,18 +43,18 @@
 
 
         <!-- Activate your account button -->
-        <a href="<?php echo $profileComplete; ?>" style="background-color: #3971FA; border-radius:40px; color: #ffffff; display: block; font-size: 16px; font-weight: 600; line-height: 1.5; margin-bottom:24px; margin-top: 24px; margin-left:auto; margin-right:auto; max-width: 320px; padding: 12px 24px; text-align: center; text-decoration: none; width: 100%;">Compléter mon profil</a>
+        <a href="<?php echo $profileComplete; ?>" style="background-color: #3971FA; border-radius:40px; color: #ffffff; display: block; font-size: 16px; font-weight: 600; line-height: 1.5; margin-bottom:24px; margin-top: 24px; margin-left:auto; margin-right:auto; max-width: 320px; padding: 12px 24px; text-align: center; text-decoration: none; width: 100%;">Complete my profile</a>
 
 
 
-        <p style="font-size: 14px; font-weight: 200; color: #000000; margin-top: 20px; padding: 0; text-align: left;">A bientôt, <br> L'équipe My Freelance ☕️</p>
+        <p style="font-size: 14px; font-weight: 200; color: #000000; margin-top: 20px; padding: 0; text-align: left;">See you soon, <br> My Freelance Team ☕️</p>
 
 
     </div>  
     <!-- Footer -->
     <div style="border-top: 1px solid #E5E5E5; margin-top: 40px; padding-top:20px;">
         <p style="font-size: 14px; font-weight: 100; color: #000000; margin: 0; padding: 0; text-align: center;">My Freelance</p>
-        <p style="font-size: 14px; font-weight: 100; color: #000000; margin: 0; padding: 0; text-align: center;">© 2024 My Freelance. Tous droits réservés.</p>
+        <p style="font-size: 14px; font-weight: 100; color: #000000; margin: 0; padding: 0; text-align: center;">© 2024 My Freelance. All rights reserved.</p>
         <!-- Logo instagram, youtube and linkedin -->
         <div style="text-align:center; margin-top:20px;">
             <a href="https://www.instagram.com/cafecreme_agency" style="margin-right: 10px;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png" alt="Instagram" width="30" style="width:30px;"></a>
